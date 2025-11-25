b7864a42712fc4276b25d8975b2028d17f5e017b
DogeGT <Doge846YT@gmail.com>
2025-11-25 17:30:12 -0400

    Update Movement.cs adding speedboost

--- Mods/Settings/Movement.cs
+++ Mods/Settings/Movement.cs
@@ -1,4 +1,4 @@
-﻿using static StupidTemplate.Menu.Main;
+using static StupidTemplate.Menu.Main;
 
 namespace StupidTemplate.Mods.Settings
 {
@@ -8,6 +8,20 @@ namespace StupidTemplate.Mods.Settings
         public static float flySpeed = 15f;
 
         public static void ChangeFlySpeed()
+        {
+            string[] speedNames = new string[] { "Very Slow", "Slow", "Normal", "Fast", "Very Fast", "Extreme" };
+            float[] speedValues = new float[] { 5f, 10f, 15f, 20f, 30f, 50f };
+
+            flySpeedIndex++;
+            flySpeedIndex %= speedNames.Length;
+
+            GetIndex("Change Fly Speed").overlapText = $"Change Fly Speed [{speedNames[flySpeedIndex]}]";
+        }
+
+        public static int speedBoostSpeedIndex = 2;
+        public static float speedBoostSpeed = 15f;
+
+        public static void ChangeSpeedBoostSpeed()
         {
             string[] speedNames = new string[] { "Very Slow", "Slow", "Normal", "Fast", "Very Fast", "Extreme" };
             float[] speedValues = new float[] { 5f, 10f, 15f, 20f, 30f, 50f };
