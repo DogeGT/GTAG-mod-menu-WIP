28c91907ff550631d20a229ff871d99cbbc05e38
DogeGT <Doge846YT@gmail.com>
2025-11-25 16:38:16 -0400

    Update Settings.cs makeing my style

--- Menu/Settings.cs
+++ Menu/Settings.cs
@@ -5,36 +5,11 @@ namespace StupidTemplate
 {
     public class Settings
     {
-        /*
-         * These are the settings for the menu.
-         * 
-         * To change the colors, you need to modify the ExtGradient variables.
-         * Here are some examples on how to use ExtGradient:
-         * 
-         * Solid Color:
-         *  new ExtGradient { colors = ExtGradient.GetSolidGradient(Color.black) }
-         *  
-         * Simple Gradient:
-         *  new ExtGradient { colors = ExtGradient.GetSimpleGradient(Color.black, Color.white) }
-         * 
-         * Rainbow Color:
-         *   new ExtGradient { rainbow = true }
-         *   
-         * Epileptic Color (random color every frame):
-         *   new ExtGradient { epileptic = true }
-         *   
-         * Self Color:
-         *   new ExtGradient { copyRigColor = true }
-         *   
-         * To change the font, you may use the following code:
-         *   Font.CreateDynamicFontFromOSFont("Comic Sans MS", 24)
-         */
-
         public static ExtGradient backgroundColor = new ExtGradient { rainbow = true};
         public static ExtGradient[] buttonColors = new ExtGradient[]
         {
-            new ExtGradient { colors = ExtGradient.GetSolidGradient(Color.black) }, // Disabled
-            new ExtGradient { rainbow = true } // Enabled
+            new ExtGradient { colors = ExtGradient.GetSimpleGradient(Color.black Color.yellow) },
+            new ExtGradient { rainbow = false } // Enabled
         };
         public static Color[] textColors = new Color[]
         {
@@ -51,7 +26,7 @@ namespace StupidTemplate
 
         public static KeyCode keyboardButton = KeyCode.Q;
 
-        public static Vector3 menuSize = new Vector3(0.1f, 1f, 1f); // Depth, width, height
+        public static Vector3 menuSize = new Vector3(0.1f, 1f, 1f);
         public static int buttonsPerPage = 8;
 
         public static float gradientSpeed = 0.5f; // Speed of colors
