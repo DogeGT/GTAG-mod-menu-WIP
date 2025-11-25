583eedaae5458f9c3536e43aa34e3c8f941492c2
DogeGT <Doge846YT@gmail.com>
2025-11-25 17:31:15 -0400

    Update Movement.cs adding speedboost

--- Mods/Movement.cs
+++ Mods/Movement.cs
@@ -1,14 +1,16 @@
-﻿using GorillaLocomotion;
+using GorillaLocomotion;
 using StupidTemplate.Classes;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.XR;
 using static StupidTemplate.Menu.Main;
+using static StupidTemplate.Mods.Settings.Movement;
 
 namespace StupidTemplate.Mods
 {
     public class Movement
     {
-        public static void Fly() // kinda broken ripped straight from ii's Stupid Template
+        public static void Fly()
         {
             if (ControllerInputPoller.instance.rightControllerPrimaryButton)
             {
@@ -17,69 +19,69 @@ namespace StupidTemplate.Mods
             }
         }
 
-    public static GameObject platl;
-    public static GameObject platr;
+        public static GameObject platl;
+        public static GameObject platr;
 
-    public static bool leftWasGrab = false;
-    public static bool rightWasGrab = false;
+        public static bool leftWasGrab = false;
+        public static bool rightWasGrab = false;
 
-    public static void Platforms() // kinda broken, and modified so it works. from ii's Stupid Template
-    {
-        if (ControllerInputPoller.instance.leftGrab && !leftWasGrab)
+        public static void Platforms()
         {
-            if (platl == null)
+            if (ControllerInputPoller.instance.leftGrab && !leftWasGrab)
             {
-                platl = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                platl.transform.localScale = new Vector3(0.025f, 0.3f, 0.4f);
-                platl.transform.position = TrueLeftHand().position;
-                platl.transform.rotation = TrueLeftHand().rotation;
+                if (platl == null)
+                {
+                    platl = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    platl.transform.localScale = new Vector3(0.025f, 0.3f, 0.4f);
+                    platl.transform.position = TrueLeftHand().position;
+                    platl.transform.rotation = TrueLeftHand().rotation;
 
-                FixStickyColliders(platl);
+                    FixStickyColliders(platl);
 
-                ColorChanger colorChanger = platl.AddComponent<ColorChanger>();
-                colorChanger.colors = StupidTemplate.Settings.backgroundColor;
+                    ColorChanger colorChanger = platl.AddComponent<ColorChanger>();
+                    colorChanger.colors = StupidTemplate.Settings.backgroundColor;
+                }
             }
-        }
-        if (!ControllerInputPoller.instance.leftGrab && leftWasGrab)
-        {
-            if (platl != null)
+            if (!ControllerInputPoller.instance.leftGrab && leftWasGrab)
             {
-                Object.Destroy(platl);
-                platl = null;
+                if (platl != null)
+                {
+                    Object.Destroy(platl);
+                    platl = null;
+                }
             }
-        }
 
-        if (ControllerInputPoller.instance.rightGrab && !rightWasGrab)
-        {
-            if (platr == null)
+            if (ControllerInputPoller.instance.rightGrab && !rightWasGrab)
             {
-                platr = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                platr.transform.localScale = new Vector3(0.025f, 0.3f, 0.4f);
-                platr.transform.position = TrueRightHand().position;
-                platr.transform.rotation = TrueRightHand().rotation;
+                if (platr == null)
+                {
+                    platr = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    platr.transform.localScale = new Vector3(0.025f, 0.3f, 0.4f);
+                    platr.transform.position = TrueRightHand().position;
+                    platr.transform.rotation = TrueRightHand().rotation;
 
-                FixStickyColliders(platr);
+                    FixStickyColliders(platr);
 
-                ColorChanger colorChanger = platr.AddComponent<ColorChanger>();
-                colorChanger.colors = StupidTemplate.Settings.backgroundColor;
+                    ColorChanger colorChanger = platr.AddComponent<ColorChanger>();
+                    colorChanger.colors = StupidTemplate.Settings.backgroundColor;
+                }
             }
-        }
 
-        if (!ControllerInputPoller.instance.rightGrab && rightWasGrab) // grab released
-        {
-            if (platr != null)
+            if (!ControllerInputPoller.instance.rightGrab && rightWasGrab) // grab released
             {
-                Object.Destroy(platr);
-                platr = null;
+                if (platr != null)
+                {
+                    Object.Destroy(platr);
+                    platr = null;
+                }
             }
-        }
 
-        leftWasGrab = ControllerInputPoller.instance.leftGrab;
-        rightWasGrab = ControllerInputPoller.instance.rightGrab;
-    }
+            leftWasGrab = ControllerInputPoller.instance.leftGrab;
+            rightWasGrab = ControllerInputPoller.instance.rightGrab;
+        }
 
         public static bool previousTeleportTrigger;
-        public static void TeleportGun() // also kinda broken, and ripped straight from ii's Stupid Template
+        public static void TeleportGun()
         {
             if (ControllerInputPoller.instance.rightGrab)
             {
@@ -94,6 +96,66 @@ namespace StupidTemplate.Mods
 
                 previousTeleportTrigger = ControllerInputPoller.TriggerFloat(XRNode.RightHand) > 0.5f;
             }
+        }
+
+        public static void WASDFLY()
+        {
+            bool isMoving;
+
+            isMoving =
+            Keyboard.current.wKey.isPressed ||
+            Keyboard.current.aKey.isPressed ||
+            Keyboard.current.sKey.isPressed ||
+            Keyboard.current.dKey.isPressed ||
+            Keyboard.current.spaceKey.isPressed ||
+            Keyboard.current.leftCtrlKey.isPressed;
+
+            Rigidbody rb = GorillaLocomotion.GTPlayer.Instance.GetComponent<Rigidbody>();
+
+            Vector3 moveDir = Vector3.zero;
+
+            // Forward
+            if (Keyboard.current.wKey.isPressed)
+                moveDir += GorillaLocomotion.GTPlayer.Instance.headCollider.transform.forward;
+
+            // Backward
+            if (Keyboard.current.sKey.isPressed)
+                moveDir -= GorillaLocomotion.GTPlayer.Instance.headCollider.transform.forward;
+
+            // Right
+            if (Keyboard.current.dKey.isPressed)
+                moveDir += GorillaLocomotion.GTPlayer.Instance.headCollider.transform.parent.right;
+
+            // Left
+            if (Keyboard.current.aKey.isPressed)
+                moveDir -= GorillaLocomotion.GTPlayer.Instance.headCollider.transform.parent.right;
+
+            // Up
+            if (Keyboard.current.spaceKey.isPressed)
+                moveDir += GorillaLocomotion.GTPlayer.Instance.headCollider.transform.up;
+
+            // Down
+            if (Keyboard.current.ctrlKey.isPressed)
+                moveDir -= GorillaLocomotion.GTPlayer.Instance.headCollider.transform.up;
+
+            if (!isMoving)
+                rb.linearVelocity = Vector3.zero;
+                rb.angularVelocity = Vector3.zero;
+
+            // Apply movement
+            if (moveDir != Vector3.zero)
+            {
+                GorillaLocomotion.GTPlayer.Instance.transform.position += moveDir * Settings.Movement.flySpeed * Time.deltaTime;
+
+                // Zero velocity (new Unity API)
+                rb.linearVelocity = Vector3.zero;
+            }
+
+        }
+
+        public static void speedboost() {
+            GorillaLocomotion.GTPlayer.Instance.jumpMultiplier = speedBoostSpeed;
+            GorillaLocomotion.GTPlayer.Instance.maxJumpSpeed = speedBoostSpeed;
         }
     }
 }
