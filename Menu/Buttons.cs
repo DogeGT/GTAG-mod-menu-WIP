37963b09ab130705a9c9e738e34acbee5ad19efe
DogeGT <Doge846YT@gmail.com>
2025-11-27 11:45:06 -0400

    Update Buttons.cs speedboost update

--- Menu/Buttons.cs
+++ Menu/Buttons.cs
@@ -7,17 +7,6 @@ namespace StupidTemplate.Menu
 {
     public class Buttons
     {
-        /*
-         * Here is where all of your buttons are located.
-         * To create a button, you may use the following code:
-         * 
-         * Move to Category:
-         *   new ButtonInfo { buttonText = "Settings", method =() => currentCategory = 1, isTogglable = false, toolTip = "Opens the main settings page for the menu."},
-         *   new ButtonInfo { buttonText = "Return to Main", method =() => currentCategory = 0, isTogglable = false, toolTip = "Returns to the main page of the menu."},
-         * 
-         * Togglable Mod:
-         *   new ButtonInfo { buttonText = "Platforms", method =() => Movement.Platforms(), toolTip = "Spawns platforms on your hands when pressing grip."},
-         */
 
         public static ButtonInfo[][] buttons = new ButtonInfo[][]
         {
@@ -63,6 +52,7 @@ namespace StupidTemplate.Menu
                 new ButtonInfo { buttonText = "Platforms", method =() => Movement.Platforms(), toolTip = "Spawns platforms on your hands when pressing grip."},
                 new ButtonInfo { buttonText = "Fly", method =() => Movement.Fly(), toolTip = "Sends you forward when holding A."},
                 new ButtonInfo { buttonText = "Teleport Gun", method =() => Movement.TeleportGun(), toolTip = "Teleports you to wherever your pointer is when pressing trigger."},
+                new ButtonInfo { buttonText = "Speedboost", method =() => Movement.speedboost(), toolTip = "mak u FAST or slow"},
             },
 
             new ButtonInfo[] { // Advantages Mods [6]
